9d994333327a17f5fe13ab271df930aeb60b2679
Ronny Endara <rojaence@gmail.com>
2025-08-10 17:43:03 -0500

    add: response helloWorld and enable CORS

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
+import { HttpResponse } from './common/helpers/http-response';
+import { TranslationService } from './common/helpers/i18n-translation';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly translation: TranslationService,
+  ) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello() {
+    return HttpResponse.success({
+      statusCode: HttpStatus.OK,
+      data: this.appService.getHello(),
+      message: this.translation.t('validation.httpMessages.success') as string,
+    });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
